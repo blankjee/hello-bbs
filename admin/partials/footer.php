c5bf154a67a677c9aea2f38af27a24f9d0118888
HAG_blank <1257749706@qq.com>
2019-08-07 22:42:20 +0800

    Version 1.0

---
+++ admin/partials/footer.php
@@ -0,0 +1,8 @@
+<div class="row">
+    <div class="col-lg-12">
+        <div class="footer">
+            <p>BBS论坛系统&nbsp;▪&nbsp;<strong>Copyright</strong> 安庆师范大学计算机与信息学院 &copy; 2019&nbsp;
+                ▪&nbsp;By：黄杰</p>
+        </div>
+    </div>
+</div>
---
+++ admin/partials/navigate.php
@@ -0,0 +1,43 @@
+<!-- sidebar -->
+<div class="sidebar sidebar-hide-to-small sidebar-shrink sidebar-gestures">
+    <div class="nano">
+        <div class="nano-content">
+            <ul>
+                <li class="active"><a href="index.php" class=""><i class="ti-home"></i> 主页 </a></li>
+
+                <li><a class="sidebar-sub-toggle"><i class="ti-user"></i> 用户管理 <span class="sidebar-collapse-icon ti-angle-down"></span></a>
+                    <ul>
+                        <li><a href="user.php">用户信息管理</a></li>
+                        <li><a href="adduser.php">添加用户信息</a></li>
+
+                    </ul>
+                </li>
+
+                <!-- 暂时不要此功能
+                <li><a class="sidebar-sub-toggle"><i class="ti-receipt"></i> 板块管理 <span class="sidebar-collapse-icon ti-angle-down"></span></a>
+                    <ul>
+                        <li><a href="area.php">板块信息管理</a></li>
+                        <li><a href="addarea.php">添加板块信息</a></li>
+
+                    </ul>
+                </li>
+                -->
+                <li><a class="sidebar-sub-toggle"><i class="ti-receipt"></i> 话题管理 <span class="sidebar-collapse-icon ti-angle-down"></span></a>
+                    <ul>
+                        <li><a href="topic.php">话题信息管理</a></li>
+                        <li><a href="addtopic.php">添加话题信息</a></li>
+
+                    </ul>
+                </li>
+
+                <!-- <li><a class="sidebar-sub-toggle"><i class="ti-search"></i>信息查询 </a>
+                 </li> -->
+                <!-- <li><a class="sidebar-sub-toggle"><i class="ti-settings"></i>其他设置  <span class="sidebar-collapse-icon ti-angle-down"></span></a>
+                -->
+
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- /# sidebar -->
